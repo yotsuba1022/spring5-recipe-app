caf14a5b653279a16d57ad018480c7f3a80eb77f
Carl <yotsuba1022@gmail.com>
2018-04-22 23:20:01 +0800

    Add unit test for IndexController.

---
+++ src/test/java/idv/carl/recipe/controllers/IndexControllerTest.java
@@ -0,0 +1,44 @@
+package idv.carl.recipe.controllers;
+
+import idv.carl.recipe.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Carl Lu
+ */
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String expected = "index";
+        String actual = indexController.getIndexPage(model);
+
+        assertEquals(expected, actual);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
