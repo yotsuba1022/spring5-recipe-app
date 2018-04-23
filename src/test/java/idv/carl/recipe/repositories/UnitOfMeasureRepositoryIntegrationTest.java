7fab4b9890b53eb8556a89196e5734ac9ff1af1b
Carl <yotsuba1022@gmail.com>
2018-04-24 00:10:09 +0800

    Add simple example for repository integration test.

---
+++ src/test/java/idv/carl/recipe/repositories/UnitOfMeasureRepositoryIntegrationTest.java
@@ -0,0 +1,36 @@
+package idv.carl.recipe.repositories;
+
+import idv.carl.recipe.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Carl Lu
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIntegrationTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void findByDescription() {
+        String expectedDesc = "Dash";
+        Optional<UnitOfMeasure> actual = unitOfMeasureRepository.findByDescription("Dash");
+        assertEquals(expectedDesc, actual.get().getDescription());
+    }
+
+}
