44a18d122c245e9ced3c4ae71555e678307be7c4
Carl <yotsuba1022@gmail.com>
2018-04-27 00:21:12 +0800

    Implement display a recipe by id.

---
+++ src/main/java/idv/carl/recipe/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package idv.carl.recipe.controllers;
+
+import idv.carl.recipe.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Carl Lu
+ */
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable long id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(id));
+        return "recipe/show";
+    }
+
+}
--- src/test/java/idv/carl/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/idv/carl/recipe/services/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 /**
@@ -24,15 +26,29 @@ public class RecipeServiceImplTest {
     private RecipeRepository recipeRepository;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
+    public void testGetRecipeById() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe actual = recipeService.findById(1L);
+        assertNotNull("Null recipe returned.", actual);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() {
         Recipe recipe = new Recipe();
-        HashSet recipeData = new HashSet();
+        HashSet<Recipe> recipeData = new HashSet();
         recipeData.add(recipe);
 
         when(recipeRepository.findAll()).thenReturn(recipeData);
